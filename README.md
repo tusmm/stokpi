aa72682867e9cffddb4530085aba040fd81cb9ce
tusmm <r.ong40915@gmail.com>
2025-08-31 11:52:27 -0400

    stock client for next time

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # stokpi
 
 https://github.com/fastapi/full-stack-fastapi-template/tree/master/backend/app/core
+https://github.com/polygon-io/client-python
+
+```cmd
+.\.venv\Scripts\Activate.ps1
+```
---
+++ backend/app/service/stock_dao.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from polygon import RESTClient
+
+
+class StockDAO():
+    def __init__(self):
+        load_dotenv()
+        POLYGON_API_KEY = os.getenv('POLYGON_API_KEY')
+
+        client = RESTClient(api_key=POLYGON_API_KEY)
+
